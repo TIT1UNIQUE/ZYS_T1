2cb84bf209b42fd562259ad08cf4e5df2c9a7ba6
Aieylisyh <Aieylisyh@users.noreply.github.com>
2025-12-04 17:50:53 +0800

    no message

---
+++ ZhuYanshu Project/Assets/Game/Scripts/game/VIC/VicDebugger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Game.Scripts.game.VIC
+{
+    public class VicDebugger : MonoBehaviour
+    {
+        public void OnClkSmallBtn1()
+        {
+
+        }
+        public void OnClkSmallBtn2()
+        {
+
+        }
+        public void OnClkSmallBtn3()
+        {
+
+        }
+        public void OnClkBigBtn1()
+        {
+
+        }
+        public void OnClkBigBtn2()
+        {
+
+        }
+
+        public void OnPressKey_1()
+        {
+
+        }
+
+        public void OnPressKey_2()
+        {
+
+        }
+
+        public void OnPressKey_3()
+        {
+
+        }
+    }
+}
